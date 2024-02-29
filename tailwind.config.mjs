47f82a9992f3cc90a6b7e6201a94fadabd9a5f1c
Belen Almazan <almazanbelen01@gmail.com>
2024-02-29 13:08:11 -0300

    Portafolio terminado

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				violet: {
+					300: '#43219C'
+				}
+			}
+		},
+	},
+	plugins: [],
+}
